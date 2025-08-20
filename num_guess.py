86b591eb0532d960d5d967b60d7492e686924446
tejas <163300834+tejaspadul@users.noreply.github.com>
2025-08-20 18:28:51 +0530

    Add files via upload

---
+++ num_guess.py
@@ -0,0 +1,19 @@
+import random
+num_to_guess=random.randint(1,100)
+attempt=1
+max_attempt=5
+while attempt<=max_attempt:
+    guess=int(input("enter guess from(1,100) :"))
+    attempt+=1
+    if guess<0 or guess>100:
+        print("enter valid guess between(1-100)")
+    if guess==num_to_guess:
+        print(f"correct.. you guessed it in {attempt} attempts")
+        break
+    elif guess<num_to_guess:
+        print("num is to low...")
+    else:
+        print("number is to high...")
+
+    if attempt==max_attempt and guess!=num_to_guess:
+        print(f"game over! the correct number was :{num_to_guess} you lose!!")
